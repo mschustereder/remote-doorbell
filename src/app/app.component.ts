95989f407b94cded3ee32c8eb092d91962721579
Mathias Schustereder <schustereder@student.tugraz.at>
2023-06-10 20:52:21 +0200

    Create website and build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'remote-doorbell';
+
+  ringDoorbell() {
+    // @todo make http call
+    console.log("ringing...");
+  }
 }
